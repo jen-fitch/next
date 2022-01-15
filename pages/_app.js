2136bd173578878e89a0d0d5ac5bd24b03ec3ea9
Jen Rees <jf247@humboldt.edu>
2022-01-15 00:11:21 -0800

    add a wrapper class to _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
 import '../styles/globals.css'
 
+const Layout = ({ children }) => <div className="page">{children}</div>
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+  )
 }
 
 export default MyApp
